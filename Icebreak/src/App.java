ecf4eda32f613e697d36ebb757a698f3ed995624
Marco <marcoguida724@gmail.com>
2020-04-10 18:16:23 -0400

    Added static class to handle database queries

--- Icebreak/src/App.java
+++ Icebreak/src/App.java
@@ -1,13 +1,19 @@
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
+import java.sql.*;
 
 public class App {
 
 	public static void main(String[] args) {
+		DatabaseManager.connectToDatabase();
+		
+		if(DatabaseManager.con == null) {
+			System.exit(0);
+		}
+		
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				JFrame frame = new LoginFrame("Icebreak");
-//				JFrame frame = new SignupFrame("Icebreak");
+				JFrame frame = new MainFrame("Icebreak", "adsda");
 				frame.setSize(750, 500);
 				frame.setResizable(false);
 				frame.setLocationRelativeTo(null);
@@ -17,6 +23,75 @@ public class App {
 		});
 		
 
+	}
+	
+	public static class DatabaseManager {
+		public static Connection con = null;
+		
+		public static void connectToDatabase() {
+			if(con == null) {
+				try {
+					String url = "jdbc:postgresql://comp421.cs.mcgill.ca:5432/cs421";
+					String username = "cs421g07";
+					String password = "dbdbSQSQ2007";
+					
+					Class.forName("org.postgresql.Driver");
+					
+					con = DriverManager.getConnection(url, username, password);
+					System.out.println("Successfully connected to database");
+				}catch(SQLException | ClassNotFoundException e) {
+					System.out.println("Successfully connected to database");
+				}
+			}else {
+				System.out.println("Already connected to database");
+			}
+		}
+		
+		public static String login(String email, String password) {
+			if(email.isBlank() || email.isEmpty() || email == null || password.isBlank() || password.isEmpty() || password == null) {
+				try {
+					Statement stmt;
+					stmt = con.createStatement();
+					String sql;
+					sql = "SELECT email FROM Users WHERE email = " + email + " AND password = " + password;
+					ResultSet rs = stmt.executeQuery(sql);
+					
+					if(rs.next()) {
+						return rs.getString("email");
+					}
+					
+					return null;
+				} catch (SQLException e) {
+					System.out.println("Error logging in to user account");
+					return null;
+				}
+			}
+			
+			return null;
+		}
+		
+		public static boolean signup() {
+			//return success status
+			return false;
+		}
+		
+		public static int match() {
+			//return chatid
+			return -1;
+		}
+		
+		public static void closeConnection() {
+			if(con != null) {
+				try {
+					con.close();
+					System.out.println("Successfully closed connection to database");
+				}catch(SQLException e) {
+					System.out.println("Error closing connection to database");
+				}
+			}else {
+				System.out.println("No connection to close");
+			}
+		}
 	}
 
 }
--- Icebreak/src/MainFrame.java
+++ Icebreak/src/MainFrame.java
@@ -1,7 +1,9 @@
-import java.awt.Container;
+import java.awt.*;
+
 
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class MainFrame extends JFrame {
@@ -10,9 +12,12 @@ public class MainFrame extends JFrame {
 		Container pane = getContentPane();
 		pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
 		DefaultListModel chatList = new DefaultListModel();
+		
+		JButton matchButton = new JButton("FIND MATCH NOW");
+		matchButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        pane.add(matchButton);
 		//TODO 
 		//for (chat c : chats){ chatList.addElement(chat.otherUserName())
-		
 	}
 
 }
